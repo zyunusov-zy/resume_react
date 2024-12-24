9cf21fbaa05bb9a7cc4ec04952637276b7acee12
Zafarbek Yunusov <zyunusov30@gmail.com>
2024-12-24 18:12:51 +0500

    done sideBar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,47 @@
 import '../styles/App.css'
 import SideBar from './SideBar.jsx'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [isExplorerOpen, setIsExplorerOpen] = useState(false);
+
+  const toggleExplorer = () => {
+    setIsExplorerOpen(!isExplorerOpen);
+  };
+
+  const handleResize = () => {
+    if (window.innerWidth <= 768) { 
+      if (isExplorerOpen) {
+        setIsExplorerOpen(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [isExplorerOpen]); 
+
 
   return (
-    <>
-      <SideBar/>
-    </>
+    <div className={`grid-app ${isExplorerOpen ? 'explorer-open' : ''}`}>
+      <div className="side-bar">
+        <SideBar toggleExplorer={toggleExplorer} />
+      </div>
+      {isExplorerOpen && (<div className='explorer'>explorer current</div>)}
+      <div className="main-content">
+        <div className="top-bar">
+
+        </div>
+
+      </div>
+      <div className="status-bar">
+
+      </div>
+    </div>
   )
 }
 
